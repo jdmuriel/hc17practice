255c2cb6b515622e75a77e325814b50dd57b8802
jdominguez <jesus.dominguez@digibis.com>
2017-02-23 19:10:26 +0100

    Problem and Solution Kotlin

---
+++ src/main/java/Data.kt
@@ -0,0 +1,20 @@
+
+class Problem {
+    var videos: Int = 0
+    var endpointCount: Int = 0
+    var requests: Int = 0     //Minimum quantity of each ingredient
+    var caches: Int = 0   //Maximum cells in a slice
+    var cacheCapacity: Int = 0
+
+    var videoSize: IntArray? = null
+    var endpointLatency: IntArray? = null //Latency to data center
+    var endpointCacheLatency: Array<IntArray>? = null  //endpointCacheLatency[i][j] is latency from endpoint i to cache j
+    var endpointVideoRequests: Array<IntArray>? = null //endpointVideoRequests[i][j] is number of expected requests from endpoint i for video j
+
+}
+
+class Solution {
+    //Max size = 10000 videos x 1000 caches
+    var videoInCache: Array<BooleanArray>? = null   //videoInCache[i][j] true if video i in cache j
+
+}
